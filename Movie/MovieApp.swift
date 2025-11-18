a67946d71cb1ce981e8ce6df734b0105d7461bfa
Ibe Vangheluwe <ibe.vangheluwe@iMacL.local>
2025-11-18 10:08:10 +0100

    first commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Ibe Vangheluwe on 18/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
